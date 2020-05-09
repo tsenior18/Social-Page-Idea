7e48f6c57b636e77325b3279ff85191cd1373298
Nanhe Kumar <tomsenior18@gmail.com>
2020-05-09 14:57:37 +0100

    Checking credentials working - need to add reset of input box & Add onload

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 function updateClock() {
     var currentTime = new Date();
-    var hours = currentTime.getHours();
+    // Operating System Clock Hours for 12h clock
+    var currentHoursAP = currentTime.getHours();
+    // Operating System Clock Hours for 24h clock
+    var currentHours = currentTime.getHours();
+    // Operating System Clock Minutes
     var currentMinutes = currentTime.getMinutes();
+    // Operating System Clock Seconds
     var currentSeconds = currentTime.getSeconds();
     // Adding 0 if Minutes & Seconds is More or Less than 10
     currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
     currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
     // Picking "AM" or "PM" 12h clock if time is more or less than 12
-    var timeOfDay = (hours < 12) ? "AM" : "PM";
-    // transform clock to 12h version if needed
-    hours = (hours > 12) ? hours - 12 : hours;
-    // transform clock to 12h version after mid night
-    hours = (hours == 0) ? 12 : hours;
-    var currentTimeString = 'The local time is ' + hours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+    var timeOfDay = (currentHours < 12) ? "AM" : "PM";
+    // display first 24h clock and after line break 12h version
+    var currentTimeString = 'Your Local Time Is ' + currentHours + ":" + currentMinutes + ":" + currentSeconds + ":" + timeOfDay;
     // print clock js in div #clock.
     $(".clocks").html(currentTimeString);
 }
 $(document).ready(function () {
     setInterval(updateClock, 1000);
 });
-
 // TYPE WRITER TEXT -----------------------------------------
 var test = document.getElementById("test");
 var text = "Welcome To the Social Media landing page!"; //put your text here
@@ -31,5 +32,17 @@ window.addEventListener("load", () => {
             result += text[i];
             test.innerHTML = result;
         }, 120 * i);
+    }
+});
+// Will check the credentials for the sign in page
+
+
+$(".signbutton").click(function () {
+    var user = $('#username').val();
+    var password = $('#password').val();
+    if (user == 'tom' && password == 'tom') {
+        console.log('hello');
+    } else {
+        console.log('bye');
     }
 });
