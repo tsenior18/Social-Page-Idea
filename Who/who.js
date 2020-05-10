3e9c3ad71e4902f60fc1a54eea20f57b91fed7ba
Nanhe Kumar <tomsenior18@gmail.com>
2020-05-10 08:26:58 +0100

    all pages title & nav set up

---
+++ Who/who.js
@@ -0,0 +1,13 @@
+// TYPE WRITER TEXT -----------------------------------------
+var test = document.getElementById("test");
+var text = "Welcome To the Who Made this Page!"; //put your text here
+var result = "";
+//Press rerun button to see effect
+window.addEventListener("load", () => {
+    for (let i = 0; i < text.length; i++) {
+        setTimeout(function () {
+            result += text[i];
+            test.innerHTML = result;
+        }, 120 * i);
+    }
+});
